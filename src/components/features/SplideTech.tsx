515fe1f41f1bbd8db01b25b06c46cd1777287923
Samuel Casco <samuelcasco44@gmail.com>
2023-10-28 11:15:12 -0600

    Format changes

--- src/components/features/SplideTech.tsx
+++ src/components/features/SplideTech.tsx
@@ -9,13 +9,8 @@ import {
    SiTypescript,
    SiJava,
 } from "react-icons/si";
-// import { Tooltip as ReactTooltip, Tooltip } from "react-tooltip";
-// Default theme
 import "@splidejs/react-splide/css";
-// or other themes
-// import '@splidejs/react-splide/css/skyblue';
 import "@splidejs/react-splide/css/sea-green";
-// or only core styles
 import "@splidejs/react-splide/css/core";
 
 const iconClasses = "w-12 md:w-20 h-12 md:h-20 mx-auto align-middle";
@@ -23,94 +18,103 @@ const iconTextClasses =
    "dark:text-white text-sm md:text-base text-center pt-2 opacity-40";
 
 function SplideTech() {
+   const slideOptions: any = {
+      perPage: 3,
+      type: "loop",
+      focus: "center",
+      rewind: false,
+      autoplay: true,
+      interval: 5000,
+      width: 800,
+   };
+
+   const slides = [
+      {
+         id: "siangular",
+         href: "https://angular.io/",
+         icon: (
+            <SiAngular className={`${iconClasses}`} style={{ color: "red" }} />
+         ),
+         text: "Angular",
+      },
+      {
+         href: "https://es.reactjs.org",
+         icon: (
+            <SiReact
+               className={`${iconClasses}`}
+               style={{ color: "#00D8FF" }}
+            />
+         ),
+         text: "React",
+      },
+      {
+         href: "https://getbootstrap.com/docs/5.2",
+         icon: (
+            <SiBootstrap
+               className={`${iconClasses}`}
+               style={{ color: "#8300FF" }}
+            />
+         ),
+         text: "Bootstrap",
+      },
+      {
+         href: "https://expressjs.com",
+         icon: (
+            <SiExpress
+               className={`${iconClasses}`}
+               style={{ color: "#9A9A9A" }}
+            />
+         ),
+         text: "Express",
+      },
+      {
+         href: "https://nodejs.org",
+         icon: (
+            <SiNodedotjs
+               className={`${iconClasses}`}
+               style={{ color: "#8BC500" }}
+            />
+         ),
+         text: "NodeJS",
+      },
+      {
+         href: "https://www.typescriptlang.org",
+         icon: (
+            <SiTypescript
+               className={`${iconClasses}`}
+               style={{ color: "#3178C6" }}
+            />
+         ),
+         text: "Typescript",
+      },
+      {
+         href: "https://www.xataka.com/basics/que-html5-que-novedades-ofrece",
+         icon: (
+            <SiHtml5
+               className={`${iconClasses}`}
+               style={{ color: "#FF4C1E" }}
+            />
+         ),
+         text: "HTML5",
+      },
+      {
+         href: "https://www.java.com/",
+         icon: <SiJava className={`${iconClasses}`} style={{ color: "red" }} />,
+         text: "Java",
+      },
+   ];
+
    return (
       <div className="z-10">
-         <Splide
-            options={{
-               perPage: 3,
-               type: "loop",
-               focus: "center",
-               rewind: false,
-               autoplay: true,
-               interval: 5000,
-               width: 800,
-            }}
-         >
-            <SplideSlide id="siangular" className="mx-auto">
-               <a href="https://angular.io/" target="_blank">
-                  <SiAngular
-                     className={`${iconClasses}`}
-                     style={{ color: "red" }}
-                  />
-               </a>
-               <p className={iconTextClasses}>Angular</p>
-            </SplideSlide>
-            <SplideSlide>
-               <a href="https://es.reactjs.org" target="_blank">
-                  <SiReact
-                     className={`${iconClasses}`}
-                     style={{ color: "#00D8FF" }}
-                  />
-               </a>
-               <p className={iconTextClasses}>React</p>
-            </SplideSlide>
-            <SplideSlide>
-               <a href="https://getbootstrap.com/docs/5.2" target="_blank">
-                  <SiBootstrap
-                     className={`${iconClasses}`}
-                     style={{ color: "#8300FF" }}
-                  />
-               </a>
-               <p className={iconTextClasses}>Bootstrap</p>
-            </SplideSlide>
-            <SplideSlide>
-               <a href="https://expressjs.com" target="_blank">
-                  <SiExpress
-                     className={`${iconClasses}`}
-                     style={{ color: "#9A9A9A" }}
-                  />
-               </a>
-               <p className={iconTextClasses}>Express</p>
-            </SplideSlide>
-            <SplideSlide>
-               <a href="https://nodejs.org" target="_blank">
-                  <SiNodedotjs
-                     className={`${iconClasses}`}
-                     style={{ color: "#8BC500" }}
-                  />
-               </a>
-               <p className={iconTextClasses}>NodeJS</p>
-            </SplideSlide>
-            <SplideSlide>
-               <a href="https://www.typescriptlang.org" target="_blank">
-                  <SiTypescript
-                     className={`${iconClasses}`}
-                     style={{ color: "#3178C6" }}
-                  />
-               </a>
-               <p className={iconTextClasses}>Typescript</p>
-            </SplideSlide>
-            <SplideSlide>
-               <a
-                  href="https://www.xataka.com/basics/que-html5-que-novedades-ofrece"
-                  target="_blank"
-               >
-                  <SiHtml5
-                     className={`${iconClasses}`}
-                     style={{ color: "#FF4C1E" }}
-                  />
-               </a>
-               <p className={iconTextClasses}>HTML5</p>
-            </SplideSlide>
-            <SplideSlide>
-               <a href="https://www.java.com/" target="_blank">
-                  <SiJava
-                     className={`${iconClasses}`}
-                     style={{ color: "red" }}
-                  />
-               </a>
-               <p className={iconTextClasses}>Java</p>
-            </SplideSlide>
+         <Splide options={slideOptions}>
+            {slides.map((slide, index) => (
+               <SplideSlide key={index} id={slide.id} className="mx-auto">
+                  <a href={slide.href} target="_blank">
+                     {slide.icon}
+                  </a>
+                  <p className={iconTextClasses}>{slide.text}</p>
+               </SplideSlide>
+            ))}
          </Splide>
       </div>
    );
