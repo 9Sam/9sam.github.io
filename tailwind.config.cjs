9b834de3adca94256fa93ebe293994a419d31648
Samuel Casco <samuelcasco44@gmail.com>
2023-10-28 00:25:20 -0600

    theme colors are now extended and not replace

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,25 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-   ],
+   content: ["./src/**/*.{js,jsx,ts,tsx}"],
    darkMode: "class",
    theme: {
-      colors: {
-         transparent: 'transparent',
-         current: 'currentColor',
-         'dark': '#030312',
-         'darken-gray': '#ffff',
-         'light-gray': '#E3E2E2',
-         'white': '#ffffff',
-         'primary': '#20FFA2',
-         'secondary': '#25B6D2',
-         'tertiary': '#25B6D2',
-         'success': '#A1FF72',
-         'warning': '#FFF25B',
-         'danger': '#FF5B5B'
+      extend: {
+         colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            dark: "#030312",
+            "darken-gray": "#ffff",
+            "light-gray": "#E3E2E2",
+            white: "#ffffff",
+            "d-primary": "#20FFA2",
+            primary: "#20FFA2",
+            "d-secondary": "#25B6D2",
+            secondary: "#25B6D2",
+            "d-tertiary": "#25B6D2",
+            tertiary: "#25B6D2",
+            success: "#A1FF72",
+            warning: "#FFF25B",
+            danger: "#FF5B5B",
+         },
       },
-      extend: {},
    },
    plugins: [],
 };
