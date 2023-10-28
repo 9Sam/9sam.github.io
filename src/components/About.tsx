ad883e3458b7a01d33aa673b4bb087910463ace7
Samuel Casco <samuelcasco44@gmail.com>
2023-10-28 11:14:22 -0600

    About elements now have blur effect

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -46,13 +46,13 @@ function About() {
                );
             })}
          </div>
-         <div className="text-dark text-opacity-70 text-center rounded-md shadow bg-dark bg-opacity-5 dark:bg-white dark:bg-opacity-5 h-auto z-10">
+         <div className="text-dark text-opacity-70 text-center rounded-md shadow bg-dark bg-opacity-5 backdrop-blur-sm dark:bg-white dark:bg-opacity-5 h-auto z-10">
             <div className="flex flex-col md:flex-row">
-               <img
+               {/* <img
                   className="max-h-40 md:max-h-80 md:max-w-1/3 object-cover w-full md:w-1/3 mx-auto rounded-bl rounded-tl shadow-lg"
                   src={code}
                   alt=""
-               />
+               /> */}
                <p className="max-w-2/3 mx-auto m-auto p-5 text-lg dark:text-light-gray">
                   <i>{MessagesE.ABOUT_ME}</i>
                </p>
--- src/components/features/ShowCardMain.tsx
+++ src/components/features/ShowCardMain.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function ShowCardMain({ text, icon }: Props) {
    return (
-      <div className="w-full md:1/2 lg:w-1/3 h-28 rounded shadow bg-dark bg-opacity-5 dark:bg-white dark:bg-opacity-5 p-3">
+      <div className="w-full md:1/2 lg:w-1/3 h-28 rounded shadow bg-dark bg-opacity-5 backdrop-blur-sm dark:bg-white dark:bg-opacity-5 p-3">
          <div className="flex h-full">
             <div className="w-1/3 flex justify-center align-middle">{icon}</div>
             <div className="w-2/3 my-auto text-dark text-opacity-70 dark:text-white dark:text-opacity-90">
