39fa18768fb57f483e31bb2c4f837a2fd06de78d
Samuel Casco <samuelcasco44@gmail.com>
2023-10-30 20:27:43 -0600

    svg setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+declare const __dirname: string;
 
 // https://vitejs.dev/config/
+/// <reference types="vite-plugin-svgr/client" />
 export default defineConfig({
-  plugins: [react()],
-})
+   plugins: [
+      react(),
+      svgr({ svgrOptions: { icon: true }, include: "**/*.svg?react" }),
+   ],
+   build: {
+      commonjsOptions: {
+         include: ["tailwind-config.cjs", "node_modules/**"],
+      },
+   },
+   optimizeDeps: {
+      include: ["tailwind-config"],
+   },
+   resolve: {
+      alias: {
+         "@": path.resolve(__dirname, "./src"),
+         "tailwind-config": path.resolve(__dirname, "./tailwind.config.cjs"),
+      },
+   },
+});
