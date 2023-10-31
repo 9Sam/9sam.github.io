3d84642507915e68a65b35f25296615bfa3d7ace
Samuel Casco <samuelcasco44@gmail.com>
2023-10-30 20:28:55 -0600

    svg access files declaration for typescript

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module "tailwind-config" {
+   const config: Config;
+   export default config;
+}
+
+declare module "*.svg?react" {
+   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+   export default content;
+}
