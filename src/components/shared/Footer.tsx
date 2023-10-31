4878324c51c586c33bb9a2068f0e56cd8e56ab5b
Samuel Casco <samuelcasco44@gmail.com>
2023-10-30 20:30:57 -0600

    attribution to freepik for image

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,8 +1,17 @@
 function Footer() {
    return (
       <footer className="p-4 mt-auto bg-white shadow md:px-6 md:py-6 dark:bg-dark z-10">
-         <span className="block text-sm text-dark sm:text-center dark:text-light-gray">
+         <span className="flex flex-col md:flex-row md:justify-center text-sm text-dark sm:text-center dark:text-light-gray">
+            <div>
+               <a href="https://www.freepik.com/free-vector/desktop-computer-vconcept-illustration_32318401.htm#query=computer&position=23&from_view=search&track=sph">
+                  Image by storyset
+               </a>{" "}
+               on Freepik
+            </div>
+            <span className="ml-2">
             © 2022 Samuel Casco. All Rights Reserved.
+
+            </span>
          </span>
       </footer>
    );
