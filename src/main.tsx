7546300dad9cb7b0b7ed2463acdf526a47c35adf
Samuel Casco <samuelcasco44@gmail.com>
2023-11-04 14:51:56 -0600

    spotlight card effect

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./styles/spotlight.styles.css"
 import { DarkModeProvider } from "./context/DarkModeContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
